f172472da46fb0d90d93d5b0a7a87dfa8bce1167
fachrul-rachman <fachrulrachman21@gmail.com>
2025-12-03 15:09:49 +0700

    a

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   basePath: "/monitorchatAI",
   assetPrefix: "/monitorchatAI",
-  trailingSlash: true,
 };
 
 export default nextConfig;
