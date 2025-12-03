30e221940bff8ac9ea2f63815a28a8bb186e2eb1
fachrul-rachman <fachrulrachman21@gmail.com>
2025-12-03 15:24:41 +0700

    asdasdasd

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import { cookies } from "next/headers";
 import { ChatDashboard } from "@/components/chat-dashboard";
 import { LoginScreen } from "@/components/login-screen";
