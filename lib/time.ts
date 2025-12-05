48121426f8ca08c1cead264aa52e7f890e596dae
fachrul-rachman <fachrulrachman21@gmail.com>
2025-12-05 13:07:46 +0700

    time

--- lib/time.ts
+++ lib/time.ts
@@ -14,7 +14,9 @@ const relativeFormatter = new Intl.RelativeTimeFormat(undefined, {
 
 export function formatRelativeTime(input: string | number | Date) {
   const date = new Date(input);
-  const diff = (date.getTime() - Date.now()) / 1000;
+  // We want "how long ago" something happened, so
+  // positive numbers mean "in the past" for the formatter.
+  const diff = (Date.now() - date.getTime()) / 1000;
   let duration = diff;
 
   for (const division of DIVISIONS) {
